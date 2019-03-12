9464ec1957ef627331b796b30146d16807e36545
Óscar Marín Iglesias <UO251857@uniovi.es>
2019-03-12 16:04:59 +0100

    Enter button bug fixed. Unused code removed.

--- src/index.js
+++ src/index.js
@@ -56,6 +56,9 @@ $('#logout-btn').click(() => {
 
 $('#refresh-btn').click(checkForNotifications);
 
+/**
+ * THIS IS NOT USED
+ */
 $('#theme-btn').click(() => {
   const $modalBody = $('#theme-selector .modal-body');
   $modalBody.empty();
@@ -81,10 +84,6 @@ $('#save-theme-btn').click(() => {
 
   if (newTheme !== selectedTheme) {
     selectedTheme = newTheme;
-
-    if (semanticChat) {
-      setUpBoard(semanticChat);
-    }
   }
 
   $('#theme-selector').modal('hide');
@@ -96,168 +95,6 @@ $('#save-theme-btn').click(() => {
 function setUpForEveryChatOption() {
   $('#chat-loading').removeClass('hidden');
   $('#chat').removeClass('hidden');
-}
-
-/**
- * This method does the preparations after every Chat option has been set up.
- */
-function setUpAfterEveryChatOptionIsSetUp() {
-
-}
-
-/**
- * This method sets up a new chess Chat.
- * @returns {Promise<void>}
- */
-async function setUpNewChessChat() {
-  setUpForEveryChatOption();
-
-  const startPosition = getNewChatPosition();
-  const realTime = getRealTime();
-  semanticChat = await core.setUpNewChat(userDataUrl, userWebId, oppWebId, startPosition, chatName, dataSync, realTime);
-
-  if (realTime) {
-    let newMoveFound = false;
-    webrtc = new WebRTC({
-      userWebId,
-      userInboxUrl: await core.getInboxUrl(userWebId),
-      opponentWebId: oppWebId,
-      opponentInboxUrl: await core.getInboxUrl(oppWebId),
-      fetch: auth.fetch,
-      initiator: true,
-      onNewData: rdfjsSource => {
-        core.checkForNewMoveForRealTimeChat(semanticChat, dataSync, userDataUrl, rdfjsSource, (san, url) => {
-          semanticChat.loadMove(san, {url});
-          board.position(semanticChat.getChess().fen());
-          updateStatus();
-          newMoveFound = true;
-        });
-
-        if (!newMoveFound) {
-          core.checkForGiveUpOfRealTimeChat(semanticChat, rdfjsSource, (agentUrl, objectUrl) => {
-            semanticChat.loadGiveUpBy(agentUrl);
-            $('#real-time-opponent-quit').modal('show');
-          });
-        }
-      },
-      onCompletion: () => {
-        $('#real-time-setup').modal('hide');
-      },
-      onClosed: (closedByUser) => {
-        if (!closedByUser && !$('#real-time-opponent-quit').is(':visible')) {
-          $('#real-time-opponent-quit').modal('show');
-        }
-      }
-    });
-
-    $('#real-time-setup .modal-body ul').append('<li>Invitation sent</li>');
-    $('#real-time-setup').modal('show');
-  }
-  
-
-  setUpBoard(semanticChat);
-  setUpAfterEveryChatOptionIsSetUp();
-}
-
-/**
- * This method lets a player continue an existing chess chat.
- * @param chatUrl: the url of the chat to continue.
- * @returns {Promise<void>}
- */
-async function continueExistingChessChat(chatUrl) {
-  setUpForEveryChatOption();
-  const loader = new Loader(auth.fetch);
-  semanticChat = await loader.loadFromUrl(chatUrl, userWebId, userDataUrl);
-  oppWebId = semanticChat.getOpponentWebId();
-
-  setUpBoard(semanticChat);
-  setUpAfterEveryChatOptionIsSetUp();
-}
-
-/**
- * This method sets up the chessboard.
- * @param semanticChat: the Semantic Chat which drives the board.
- * @returns {Promise<void>}
- */
-async function setUpBoard(semanticChat) {
-  const chat = semanticChat.getChess();
-
-  // do not pick up pieces if the Chat is over
-  // only pick up pieces for the side to move
-  const onDragStart = function(source, piece, position, orientation) {
-    const userColor = semanticChat.getUserColor();
-
-    if (chat.chat_over() === true || userColor !== chat.turn()) {
-      return false;
-    }
-
-    if (chat.chat_over() === true || (userColor !== chat.turn() &&
-        ((userColor === 'w' && piece.search(/^b/) !== -1) ||
-        (userColor === 'b' && piece.search(/^w/) !== -1)))) {
-      return false;
-    }
-  };
-
-  const onDrop = async function(source, target) {
-    // see if the move is legal
-    const move = semanticChat.doMove({
-      from: source,
-      to: target,
-      promotion: 'q' // NOTE: always promote to a queen for example simplicity
-    });
-
-    // illegal move
-    if (move === null) return 'snapback';
-
-    await dataSync.executeSPARQLUpdateForUser(userDataUrl, move.sparqlUpdate);
-
-    if (move.notification) {
-      if (semanticChat.isRealTime()) {
-        // TODO send notification over data channel
-        webrtc.sendData(move.notification);
-      } else {
-        dataSync.sendToOpponentsInbox(await core.getInboxUrl(oppWebId), move.notification);
-      }
-    }
-
-    updateStatus();
-  };
-
-  // update the board position after the piece snap
-  // for castling, en passant, pawn promotion
-  const onSnapEnd = function() {
-    board.position(chat.fen());
-  };
-
-  const cfg = {
-    draggable: true,
-    onDragStart: onDragStart,
-    onDrop: onDrop,
-    onSnapEnd: onSnapEnd,
-    position: chat.fen(),
-    orientation: fullColor[semanticChat.getUserColor()],
-    pieceTheme: possibleThemes[selectedTheme].pieceTheme
-  };
-
-  board = ChessBoard('board', cfg);
-
-  $('#chat').removeClass('hidden');
-  $('#chat-loading').addClass('hidden');
-
-  $('.black-3c85d').css('background-color', possibleThemes[selectedTheme].color.black);
-  $('.white-1e1d7').css('background-color', possibleThemes[selectedTheme].color.white);
-
-  const oppName = await core.getFormattedName(oppWebId);
-
-  $('#opponent-name').text(oppName);
-
-  if (semanticChat.getName()) {
-    $('#name-of-the-chat').text(semanticChat.getName());
-  } else {
-    $('#name-of-the-chat').text(semanticChat.getUrl());
-  }
-
-  updateStatus();
 }
 
 auth.trackSession(async session => {
@@ -296,7 +133,6 @@ auth.trackSession(async session => {
     $('#chat-options').removeClass('hidden');
     $('#how-it-works').removeClass('hidden');
     userWebId = null;
-    semanticChat = null;
     board = null;
     clearInterval(refreshIntervalId);
     refreshIntervalId = null;
@@ -322,35 +158,43 @@ $('#new-btn').click(async () => {
     for await (const friend of friendList) {
         $('#possible-people').append('<option value='+friend.username+'>'+friend.name+'</option>');
     }
-
-   $('#new-chat-options').removeClass('hidden');
-   //$('#data-url').prop('value', core.getDefaultDataUrl(userWebId));
-
+    
+    $("#data-name").keydown(function (e) {
+      if (e.keyCode == 13) {
+        sendMessage();
+      }
+    });
+    $('#new-chat-options').removeClass('hidden');
   } else {
     $('#login-required').modal('show');
   }
 });
 
 $('#start-new-chat-btn').click(async () => {
+   await sendMessage();
+});
+
+async function sendMessage(){
   var message = $('#data-name').val();
   var a = $("#possible-people option:selected").val();
   var receiver = core.getFriendOfList(friendList, a);
   var intro1 = "<chatting with "+userWebId+">\n"
   var intro2 = "<chatting with "+receiver.card+">\n"
   try {
-	
-	
     dataSync.sendToOpponentsInbox(receiver.inbox, intro1 + message);
     dataSync.sendToOpponentsInbox("https://"+myUsername+".solid.community/inbox/", intro2 + message);
-	document.getElementById("data-name").value = "";
-	$("#sent-messages").val($("#sent-messages").val() + "\n" + message);
+    document.getElementById("data-name").value = "";
+    if($("#sent-messages").val()=="")
+      $("#sent-messages").val(message);
+    else
+	    $("#sent-messages").val($("#sent-messages").val() + "\n" + message);
     //dataSync.createEmptyFileForUser("https://"+myUsername+".solid.community/inbox/"+receiver.username+".ttl");
     //dataSync.executeSPARQLUpdateForUser("https://"+myUsername+".solid.community/inbox/"+receiver.username+".ttl", 'INSERT DATA {'+message+'}');
   } catch (e) {
     core.logger.error(`Could not send message to the user.`);
     core.logger.error(e);
   }
-});
+}
 
 $('#join-btn').click(async () => {
   if (userWebId) {
@@ -379,82 +223,6 @@ $('#join-btn').click(async () => {
     }
   } else {
     $('#login-required').modal('show');
-  }
-});
-
-$('#join-chat-btn').click(async () => {
-  if ($('#join-data-url').val() !== userWebId) {
-    userDataUrl = $('#join-data-url').val();
-
-    if (await core.writePermission(userDataUrl, dataSync)){
-      $('#join-chat-options').addClass('hidden');
-      const chatUrl = $('#chat-urls').val();
-
-      let i = 0;
-
-      while (i < chatsToJoin.length && chatsToJoin[i].chatUrl !== chatUrl) {
-        i++;
-      }
-
-      const chat = chatsToJoin[i];
-
-      // remove it from the array so it's no longer shown in the UI
-      chatsToJoin.splice(i, 1);
-
-      afterChatSpecificOptions();
-      setUpForEveryChatOption();
-      oppWebId = chat.opponentWebId;
-      semanticChat = await core.joinExistingChessChat(chatUrl, chat.invitationUrl, oppWebId, userWebId, userDataUrl, dataSync, chat.fileUrl);
-
-      if (semanticChat.isRealTime()) {
-        webrtc = new WebRTC({
-          userWebId,
-          userInboxUrl: await core.getInboxUrl(userWebId),
-          opponentWebId: oppWebId,
-          opponentInboxUrl: await core.getInboxUrl(oppWebId),
-          fetch: auth.fetch,
-          initiator: false,
-          onNewData: rdfjsSource => {
-            let newMoveFound = false;
-
-            core.checkForNewMoveForRealTimeChat(semanticChat, dataSync, userDataUrl, rdfjsSource, (san, url) => {
-              semanticChat.loadMove(san, {url});
-              board.position(semanticChat.getChess().fen());
-              updateStatus();
-              newMoveFound = true;
-            });
-
-            if (!newMoveFound) {
-              core.checkForGiveUpOfRealTimeChat(semanticChat, rdfjsSource, (agentUrl, objectUrl) => {
-                semanticChat.loadGiveUpBy(agentUrl);
-                $('#real-time-opponent-quit').modal('show');
-              });
-            }
-          },
-          onCompletion: () => {
-            $('#real-time-setup').modal('hide');
-          },
-          onClosed: (closedByUser) => {
-            if (!closedByUser && !$('#real-time-opponent-quit').is(':visible')) {
-              $('#real-time-opponent-quit').modal('show');
-            }
-          }
-        });
-
-        webrtc.start();
-
-        $('#real-time-setup .modal-body ul').append('<li>Response sent</li><li>Setting up direct connection</li>');
-        $('#real-time-setup').modal('show');
-      }
-
-      setUpBoard(semanticChat);
-      setUpAfterEveryChatOptionIsSetUp();
-    } else {
-      $('#write-permission-url').text(userDataUrl);
-      $('#write-permission').modal('show');
-    }
-  } else {
-    console.warn('We are pretty sure you do not want to remove your WebID.');
   }
 });
 
@@ -536,53 +304,16 @@ $('#continue-chat-btn').click(async () => {
 });
 
 /**
- * This method updates the status of the chat in the UI.
- */
-function updateStatus() {
-  const statusEl = $('#status');
-  let status = '';
-  const chat = semanticChat.getChess();
-
-  let moveColor = 'White';
-
-  if (chat.turn() === 'b') {
-    moveColor = 'Black';
-  }
-
-  // checkmate?
-  if (chat.in_checkmate() === true) {
-    status = 'chat over, ' + moveColor + ' is in checkmate.';
-  }
-
-  // draw?
-  else if (chat.in_draw() === true) {
-    status = 'chat over, drawn position';
-  }
-
-  // chat still on
-  else {
-    status = moveColor + ' to move';
-
-    // check?
-    if (chat.in_check() === true) {
-      status += ', ' + moveColor + ' is in check';
-    }
-  }
-
-  statusEl.html(status);
-}
-
-
-/**
  * This method checks if a new move has been made by the opponent.
  * The necessarily data is stored and the UI is updated.
  * @returns {Promise<void>}
  */
+
 async function checkForNotifications() {
   console.log('Checking for new notifications');
 
   const updates = await core.checkUserInboxForUpdates(await core.getInboxUrl(userWebId));
-
+/*
   updates.forEach(async (fileurl) => {
     let newMoveFound = false;
     // check for new moves
@@ -608,68 +339,8 @@ async function checkForNotifications() {
         }
       }
     }
-  });
-}
-
-/**
- * This method processes a response to an invitation to join a chat.
- * @param response: the object representing the response.
- * @param fileurl: the url of the file containing the notification.
- * @returns {Promise<void>}
- */
-async function processResponseInNotification(response, fileurl) {
-  const rsvpResponse = await core.getObjectFromPredicateForResource(response.responseUrl, namespaces.schema + 'rsvpResponse');
-  let chatUrl = await core.getObjectFromPredicateForResource(response.invitationUrl, namespaces.schema + 'event');
-
-  if (chatUrl) {
-    chatUrl = chatUrl.value;
-
-    if (semanticChat && semanticChat.getUrl() === chatUrl && semanticChat.isRealTime()) {
-      if (rsvpResponse.value === namespaces.schema + 'RsvpResponseYes') {
-        $('#real-time-setup .modal-body ul').append('<li>Invitation accepted</li><li>Setting up direct connection</li>');
-        webrtc.start();
-      }
-    } else {
-      let chatName = await core.getObjectFromPredicateForResource(chatUrl, namespaces.schema + 'name');
-      const loader = new Loader(auth.fetch);
-      const chatOppWebId = await loader.findWebIdOfOpponent(chatUrl, userWebId);
-      const opponentsName = await core.getFormattedName(chatOppWebId);
-
-      //show response in UI
-      if (!chatName) {
-        chatName = chatUrl;
-      } else {
-        chatName = chatName.value;
-      }
-
-      let text;
-
-      if (rsvpResponse.value === namespaces.schema + 'RsvpResponseYes') {
-        text = `${opponentsName} accepted your invitation to join "${chatName}"!`;
-      } else if (rsvpResponse.value === namespaces.schema + 'RsvpResponseNo') {
-        text = `${opponentsName} refused your invitation to join ${chatName}...`;
-      }
-
-      if (!$('#invitation-response').is(':visible')) {
-        $('#invitation-response .modal-body').empty();
-      }
-
-      if ($('#invitation-response .modal-body').text() !== '') {
-        $('#invitation-response .modal-body').append('<br/>');
-      }
-
-      $('#invitation-response .modal-body').append(text);
-      $('#invitation-response').modal('show');
-
-      dataSync.executeSPARQLUpdateForUser(await core.getStorageForChat(userWebId, chatUrl), `INSERT DATA {
-    <${response.invitationUrl}> <${namespaces.schema}result> <${response.responseUrl}>}
-  `);
-    }
-
-    dataSync.deleteFileForUser(fileurl);
-  } else {
-    console.log(`No chat url was found for response ${response.value}.`);
-  }
+  
+  });*/
 }
 
 $('#clear-inbox-btn').click(async () => {
@@ -682,55 +353,8 @@ $('#clear-inbox-btn').click(async () => {
   });
 });
 
-function stopPlaying() {
-  $('#chat').addClass('hidden');
-  $('#chat-options').removeClass('hidden');
-  $('#how-it-works').removeClass('hidden');
-  semanticChat = null;
-  board = null;
-
-  if (webrtc) {
-    setTimeout(() => {
-      webrtc.stop();
-      webrtc = null;
-    }, 1000);
-  }
-}
-
-function giveUp() {
-  const result = semanticChat.giveUpBy(userWebId);
-
-  dataSync.executeSPARQLUpdateForUser(userDataUrl, `INSERT DATA { ${result.sparqlUpdate} }`);
-  webrtc.sendData(result.notification);
-}
-
-$('#stop-playing').click(() => {
-  if (semanticChat.isRealTime()) {
-    $('#real-time-quit').modal('show');
-  } else {
-    stopPlaying();
-  }
-});
-
-$('#yes-quit-real-time-btn').click(async () => {
-  $('#real-time-quit').modal('hide');
-
-  giveUp();
-  stopPlaying();
-});
-
-$('#custom-position-chk').change(() => {
-  if ($('#custom-position-chk').prop('checked')) {
-    $('#custom-position').removeClass('hidden');
-  } else {
-    $('#custom-position').addClass('hidden');
-  }
-});
 
 $('.btn-cancel').click(() => {
-  semanticChat = null;
-  oppWebId = null;
-
   $('#chat').addClass('hidden');
   $('#new-chat-options').addClass('hidden');
   $('#join-chat-options').addClass('hidden');
@@ -738,29 +362,6 @@ $('.btn-cancel').click(() => {
   $('#chat-options').removeClass('hidden');
   $('#how-it-works').removeClass('hidden');
 });
-
-$('#opp-quit-ok-btn').click(() => {
-  $('#real-time-opponent-quit').modal('hide');
-
-  stopPlaying();
-});
-
-/**
- * This method determines what the start position is of a new chess chat based on the what the player selected in the UI.
- * @returns {*}
- */
-function getNewChatPosition() {
-  if ($('#custom-position-chk').prop('checked')) {
-    return $('#fen').val();
-  } else {
-    return null;
-  }
-};
-
-function getRealTime() {
-  return $('#real-time-chk').prop('checked');
-};
-
 
 async function getFriends() { 
   var subject = userWebId;
