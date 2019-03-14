9a7c1db0b985183001bbab4558f749542e1ff88e
MarioDiaz98 <mariodc98@gmail.com>
2019-03-14 16:40:50 +0100

    Unsolved attempts added to a folder to merge

---
+++ loader.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var launcher = express();
+var path = require('path');
+
+launcher.use(express.static(path.join(__dirname, '/src')));
+
+launcher.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+launcher.listen(8080, () => {
+  console.log('Chat active at http://localhost:8080');
+});
---
+++ test/basicTest.js
@@ -0,0 +1,8 @@
+require('chai');
+var assert = require('assert');
+
+describe('Simple test', function () {
+  it('2 plus 2 is 4', function () {
+    assert.equal(2+2, 4)
+  })
+})
