4bb9c4c28f5f7d32d5bd6c2a52b3595c59c3bc2b
MarioDiaz98 <mariodc98@gmail.com>
2019-03-20 22:59:54 +0100

    Continue with JSONLD

--- src/message.js
+++ src/message.js
@@ -1,38 +1,40 @@
 module.exports = class Message {
-  constructor(content,date) {
+  constructor(content,date,sender) {
     this.content = content;
+	this.sender = sender;
 	if (date==null)
 		this.date = new Date();
   }
 
   get sender() {
-    return this._sender;
+    return this.sender;
   }
 
   set sender(newSender) {
-    this._sender = newSender;
+    this.sender = newSender;
   }
 
   get receiver() {
-    return this._receiver;
+    return this.receiver;
   }
 
   set receiver(newReceiver) {
-    this._receiver = newReceiver;
+    this.receiver = newReceiver;
   }
 
   get content() {
-    return this._content;
+    return this.content;
   }
 
   set sender(newContent) {
-    this._content = newContent;
+    this.content = newContent;
   }
   
     serialize(){
         return JSON.stringify({
             "sch:text":this.content,
-            "sch:dateSent":this.date
+            "sch:dateSent":this.date,
+			"sch:sender":this.sender
         });
 	}
 }
